a3703774b01c30f48d3ac0559a8fedef43f61454
Daniel Braas <danielbraas@gmail.com>
2018-12-06 22:25:20 -0600

    Added a few new shiny apps

---
+++ Apps_intro/Using the shiny package for an interactive table/app.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+server <- function(input, output, session) {
+  
+  
+  
+  dat <- reactive({
+    input$myslider
+    mtcars[1:input$myslider, c("mpg", "cyl", "disp")]
+    
+  })
+  
+  output$mytable <- shiny::renderDataTable(dat())
+}
+
+ui <- basicPage(
+  h3("Interactive table using the now deprecated Shiny data table renderer"),
+  sliderInput("myslider", "Number of rows to display", min=1, max = 32, value = 5),
+  shiny::dataTableOutput("mytable")
+  
+)
+
+
+shinyApp(ui = ui, server = server)
